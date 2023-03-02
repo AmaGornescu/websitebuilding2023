23a64f51f4b97bbf29d51f64b6a0b222714079c3
Frontend JavaScript <frontendjavascript@C3-08.local>
2023-03-02 20:50:05 +0200

    curs2

--- shop/assets/js/main.js
+++ shop/assets/js/main.js
@@ -7,6 +7,25 @@ var carousel = new bootstrap.Carousel(myCarousel, {
   touch: true,
 //   ride: false (dezactiveaza autoplay)
 })
+// go to top
+const scrollBtn = document.querySelector(".gototop");
+        const btnVisibility = () => {
+    if (window.scrollY > 400) {
+        scrollBtn.style.visibility = "visible";
+    } else {
+        scrollBtn.style.visibility = "hidden";
+    }
+};
+document.addEventListener("scroll", () => {
+    btnVisibility();
+});
+scrollBtn.addEventListener("click", () => {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
+});
+
 
 
 var id;
